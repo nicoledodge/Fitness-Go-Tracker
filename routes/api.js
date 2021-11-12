362a51e43a1fd6c1df45cef80e499783e3b2736c
nicoledodge <nicoledodge5@gmail.com>
2021-11-12 14:19:05 -0600

    started filling out routes

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const workout = require("../models/workout.js");
+
+//post the workouts
+//update workout exercise with put route
+//get workut aggregate
+// delete route
+
+module.exports = router;
