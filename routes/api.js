a9bb5fea028274f47898cd01f9fe14191efe473c
nicoledodge <nicoledodge5@gmail.com>
2021-11-13 19:18:12 -0600

    post routes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const workout = require("../models/workout.js");
 
 //post the workouts
+router.post("/api/workout", ({ body }, res) => {
+    workout.create(body)
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 //update workout exercise with put route
 //get workut aggregate
 // delete route
